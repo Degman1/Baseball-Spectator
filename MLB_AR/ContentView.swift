e98c1193b0c8396fc81ebe2bc12325de159cd547
Degman1 <dgredsox@gmail.com>
2020-01-10 12:07:09 -0500

    test pushing

--- MLB_AR/ContentView.swift
+++ MLB_AR/ContentView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("\(OpenCVWrapper.openCVVersionString())")
+        Text("\(OpenCVWrapper.openCVVersionString()) Hello")
+        
     }
 }
 
