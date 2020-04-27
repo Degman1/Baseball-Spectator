5a009bae776624ba5d5ba2708e8e287a518a48ab
JCohen72 <jecohen72@gmail.com>
2020-04-27 11:29:24 -0400

    IDK

--- MLB_AR/ContentView.swift
+++ MLB_AR/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("\(OpenCVWrapper.openCVVersionString()) Hello")
-        
+        //Text("\(OpenCVWrapper.openCVVersionString()) Hello")
+        ImageViewViewController()
     }
 }
 
