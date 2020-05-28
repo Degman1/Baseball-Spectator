4481624c0aae157b18da69d6cbe4109b819842f8
Degman1 <dgredsox@gmail.com>
2020-05-28 10:13:39 -0400

    move the processing testing into a seperate view

--- Baseball-Spectator/ContentView.swift
+++ Baseball-Spectator/ContentView.swift
@@ -9,28 +9,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var imageID = 1
-    
     var body: some View {
-        //Text("\(OpenCVWrapper.openCVVersionString()) Hello")
-        //ImageViewViewController()
-//        VStack {
-//            MainView()
-//        }
-        ZStack {
-            self.testProcessing(imageID: imageID)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-            //Text("\(OpenCVWrapper.openCVVersionString())")
-            //    .background(Color.white)
-            Stepper("ImageID: \(imageID)", value: $imageID, in: 1...11)
-        }
-    }
-    
-    func testProcessing(imageID: Int) -> Image {
-        let uiimage = UIImage(named: "image\(imageID).jpg")!
-        let res = OpenCVWrapper.processImage(uiimage, expectedHomePlateAngle: HOME_PLATE_ANGLES[imageID - 1])
-        return Image(uiImage: res)
+        MainView()
     }
 }
 
---
+++ Baseball-Spectator/TestProcessingView.swift
@@ -0,0 +1,36 @@
+//
+//  TestProcessingView.swift
+//  Baseball-Spectator
+//
+//  Created by David Gerard on 5/28/20.
+//  Copyright © 2020 David Gerard. All rights reserved.
+//
+
+import SwiftUI
+
+struct TestProcessingView: View {
+    @State var imageID = 1
+    
+    var body: some View {
+        ZStack {
+            self.testProcessing(imageID: imageID)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            //Text("\(OpenCVWrapper.openCVVersionString())")
+            //    .background(Color.white)
+            Stepper("ImageID: \(imageID)", value: $imageID, in: 1...11)
+        }
+    }
+    
+    func testProcessing(imageID: Int) -> Image {
+        let uiimage = UIImage(named: "image\(imageID).jpg")!
+        let res = OpenCVWrapper.processImage(uiimage, expectedHomePlateAngle: HOME_PLATE_ANGLES[imageID - 1])
+        return Image(uiImage: res)
+    }
+}
+
+struct TestProcessingView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestProcessingView()
+    }
+}
