a9755ca7f9a5a4f68fcd34334869fc6db6fe5bc9
Degman1 <dgredsox@gmail.com>
2020-06-10 21:09:32 -0400

    create expanded player view struct and bar view for real use (not testing)

---
+++ Baseball-Spectator/Subviews/PlayerExpandedView.swift
@@ -0,0 +1,21 @@
+//
+//  PlayerExpandedView.swift
+//  Baseball-Spectator
+//
+//  Created by David Gerard on 6/10/20.
+//  Copyright © 2020 David Gerard. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlayerExpandedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlayerExpandedView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerExpandedView()
+    }
+}
---
+++ Baseball-Spectator/Subviews/PlayerInfoBarView.swift
@@ -0,0 +1,21 @@
+//
+//  PlayerInfoBarView.swift
+//  Baseball-Spectator
+//
+//  Created by David Gerard on 6/10/20.
+//  Copyright © 2020 David Gerard. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlayerInfoBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlayerInfoBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerInfoBarView()
+    }
+}
